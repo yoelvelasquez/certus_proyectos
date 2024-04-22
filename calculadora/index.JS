504974aa3d90a15d341f167c7a8e07b775da1047
yoelvelasquez <yoelv5048@gmail.com>
2024-04-22 10:47:37 -0500

    no entiendo naa

---
+++ calculadora/index.JS
@@ -0,0 +1,16 @@
+function sumar(value1,value2){
+    var result1 = value1+value2;
+    return result1;
+}
+function restar(value1,value2){
+    var result1 = value1-value2;
+    return result1;
+}
+function multiplicar(value1,value2){
+    var result1 = value1*value2;
+    return result1;
+}
+function dividir(value1,value2){
+    var result1 = value1/value2;
+    return result1;
+}
